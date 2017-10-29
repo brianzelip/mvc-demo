1e012cb309fca74ee8b8c87dc72f64ef73271a2d
Brian Zelip <bzelip@gmail.com>
2017-10-29 15:28:14 -0400

    update routes to leverage controllers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const vendorController = require('../controllers/vendorController');
 
-router.get('/', (req, res) => {
-  res.render('layout');
-});
+router.get('/', vendorController.homePage);
+router.get('/add', vendorController.addVendor);
 
 module.exports = router;
