b5963b5db8798aae5d61dc6fa6a82ae281690207
Brian Zelip <bzelip@gmail.com>
2017-10-30 07:37:12 -0400

    start createVendor controller

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -8,3 +8,11 @@ exports.homePage = (req, res) => {
 exports.addVendor = (req, res) => {
   res.render('addVendor', { title: 'Add vendor' });
 };
+
+exports.createVendor = (req, res) => {
+  res.send(req.body);
+  // const vendor = new Vendor(req.body);
+  // await vendor.save();
+  // // req.flash('success', 'Vendor saved successfully!');
+  // res.redirect('/');
+};
