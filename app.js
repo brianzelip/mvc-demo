22a8dc614eda315760bb63af5bc8bf0d75c85031
Brian Zelip <bzelip@gmail.com>
2017-10-30 07:55:25 -0400

    require and use errorHandlers in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const errorHandlers = require('./handlers/errorHandlers');
 
 // create our Express app
 const app = express();
@@ -21,20 +22,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // // After allllll that above middleware, we finally handle our own routes!
 app.use('/', routes);
 
-// // If that above routes didnt work, we 404 them and forward to error handler
-// app.use(errorHandlers.notFound);
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
 
 // // One of our error handlers will see if these errors are just validation errors
 // app.use(errorHandlers.flashValidationErrors);
 
-// // Otherwise this was a really bad error we didn't expect! Shoot eh
-// if (app.get('env') === 'development') {
-//   /* Development Error Handler - Prints stack trace */
-//   app.use(errorHandlers.developmentErrors);
-// }
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
 
-// // production error handler
-// app.use(errorHandlers.productionErrors);
+// production error handler
+app.use(errorHandlers.productionErrors);
 
 // done! we export it so we can start the site in start.js
 module.exports = app;
