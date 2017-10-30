11311176255818d65876bc3c3d52ad390ed2787c
Brian Zelip <bzelip@gmail.com>
2017-10-30 07:32:57 -0400

    connect to our db, and init Vendor model schema in start.js

--- start.js
+++ start.js
@@ -12,6 +12,19 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+
+// import all of our models (data schemas)
+// this is also known as a "singleton" - where express allows you to just require it once at the beginning, and all subsequent files get it from here.
+require('./models/Vendor');
+
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
