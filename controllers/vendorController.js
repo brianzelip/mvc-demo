7030b3f567907ccc78f19210843f42e06d5f26ec
Brian Zelip <bzelip@gmail.com>
2017-10-30 08:07:51 -0400

    make createVendor controller async/await for successful writing to theh local mongod instance 🎉

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -9,10 +9,10 @@ exports.addVendor = (req, res) => {
   res.render('addVendor', { title: 'Add vendor' });
 };
 
-exports.createVendor = (req, res) => {
-  res.send(req.body);
-  // const vendor = new Vendor(req.body);
-  // await vendor.save();
-  // // req.flash('success', 'Vendor saved successfully!');
-  // res.redirect('/');
+exports.createVendor = async (req, res) => {
+  // res.send(req.body);
+  const vendor = new Vendor(req.body);
+  await vendor.save();
+  // req.flash('success', 'Vendor saved successfully!');
+  res.redirect('/');
 };
