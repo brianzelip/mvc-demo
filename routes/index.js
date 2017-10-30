9ffe68483578321eb6c5433e78d17ebe483442e5
Brian Zelip <bzelip@gmail.com>
2017-10-30 08:06:38 -0400

    load catchErrors() and use it in routes/index.js for async call to db in the conroller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const vendorController = require('../controllers/vendorController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', vendorController.homePage);
 router.get('/add', vendorController.addVendor);
-router.post('/add', vendorController.createVendor);
+router.post('/add', catchErrors(vendorController.createVendor));
 
 module.exports = router;
