fa0b03375b3c20470019622ee2554fd7371f6616
Brian Zelip <bzelip@gmail.com>
2017-11-02 19:46:14 -0400

    tweak updateVendor controller comments, all instances of store -> vendor

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -23,30 +23,30 @@ exports.getVendors = async (req, res) => {
 };
 
 exports.editVendor = async (req, res) => {
-  // 1. Find the store given the id parameter of the url (ie: `req.params.id`)
+  // 1. Find the vendor given the id parameter of the url (ie: `req.params.id`)
   const vendor = await Vendor.findOne({ _id: req.params.id });
   // 2. Render out the edit form so the user can update the vendor data
   res.render('editVendor', { title: `Edit ${vendor.vendorName}`, vendor });
 };
 
 exports.updateVendor = async (req, res) => {
-  // 1. find and update the store
+  // 1. find and update the vendor
   const vendor = await Vendor.findOneAndUpdate(
     { _id: req.params.id },
     req.body,
     {
-      new: true, //tells findOneAndUpdate to return the new store that was
-      //just edited, not the old store like what it returns by default
+      new: true, //tells findOneAndUpdate to return the new vendor that was
+      //just edited, not the old vendor like what it returns by default
       runValidators: true //tells findOneAndUpdate to run any required statements
-      // on the form schema in the data model; ie in this case, the store name is
-      // required so if someone submits an edit that removes the store name,
+      // on the form schema in the data model; ie in this case, the vendor name is
+      // required so if someone submits an edit that removes the vendor name,
       // this option will run the validator to catch that problem (plus the trims)
     }
   ).exec(); //tells mongoose explicitly to run this query
   // req.flash(
   //   'success',
-  //   `Successfully updated <strong>${store.name}</strong>! <a href="/stores/${store.slug}">View Store →</a>`
+  //   `Successfully updated <strong>${vendor.name}</strong>! <a href="/vendors/${vendor.slug}">View vendor →</a>`
   // );
   res.redirect(`/vendors/${vendor.id}/edit`);
-  // 2. Redirect them to the store and flash them it worked
+  // 2. Redirect them to the vendor and flash them it worked
 };
