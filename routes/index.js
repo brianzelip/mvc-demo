a96eac72eda8337fd5af37225659df4c18d05e1c
Brian Zelip <bzelip@gmail.com>
2017-11-04 17:37:23 -0400

    update the controllers called in each route

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const memberController = require('../controllers/memberController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', catchErrors(vendorController.getVendors));
-router.get('/staff', catchErrors(vendorController.getVendors));
-router.get('/staff/add', vendorController.addVendor);
-router.post('/staff/add', catchErrors(vendorController.createVendor));
-router.get('/staff/:slug/edit', catchErrors(vendorController.editVendor));
-router.post('/staff/:slug/edit', catchErrors(vendorController.updateVendor));
-router.get('/staff/:slug', catchErrors(vendorController.getVendor));
+router.get('/', catchErrors(memberController.getStaff));
+router.get('/staff', catchErrors(memberController.getStaff));
+router.get('/staff/add', memberController.addMember);
+router.post('/staff/add', catchErrors(memberController.createMember));
+router.get('/staff/:slug/edit', catchErrors(memberController.editMember));
+router.post('/staff/:slug/edit', catchErrors(memberController.updateMember));
+router.get('/staff/:slug', catchErrors(memberController.getMember));
+
 module.exports = router;
