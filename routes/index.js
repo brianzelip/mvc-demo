a04504ddb51bb9dddae8692c660511415f4c1599
Brian Zelip <bzelip@gmail.com>
2017-11-05 06:29:12 -0500

    update form mixin with urlenctype and photo display section

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,19 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(memberController.getStaff));
 router.get('/staff', catchErrors(memberController.getStaff));
 router.get('/staff/add', memberController.addMember);
-router.post('/staff/add', catchErrors(memberController.createMember));
+router.post(
+  '/staff/add',
+  memberController.upload,
+  catchErrors(memberController.resize),
+  catchErrors(memberController.createMember)
+);
 router.get('/staff/:id/edit', catchErrors(memberController.editMember));
-router.post('/staff/:id/edit', catchErrors(memberController.updateMember));
+router.post(
+  '/staff/:id/edit',
+  memberController.upload,
+  catchErrors(memberController.resize),
+  catchErrors(memberController.updateMember)
+);
 router.get('/staff/:id', catchErrors(memberController.getMember));
 
 module.exports = router;
