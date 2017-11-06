60e22091c894d42c72ae5b43de1d732de6c0e4d9
Brian Zelip <bzelip@gmail.com>
2017-11-06 06:26:24 -0500

    abstracting random bg gradients to helpers.js

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@
 */
 
 // FS is a built in module to node that let's us read files from the system we're running on
-const fs = require('fs');
+// const fs = require('fs');
 
 // // moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
 // exports.moment = require('moment');
@@ -22,6 +22,40 @@ exports.dump = obj => JSON.stringify(obj, null, 2);
 // Some details about the site
 exports.siteName = `Staff Directory`;
 
+//- - function colorDice() {
+//-     const classes = ['grab0', 'grab1', 'grab2'];
+//-     function roll(min, max) {
+//-       min = Math.ceil(min);
+//-       max = Math.floor(max);
+//-       return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+//-     };
+//-     return classes[roll(0, classes.length)];
+//-   }
+exports.colors = [
+  '#0093e9',
+  '#ff2525',
+  '#85ffbd',
+  '#fffb7d',
+  '#21d4fd',
+  '#b721ff',
+  '#4158d0',
+  '#ff6a88',
+  '#ffe32c',
+  '#b6df35',
+  '#fa8bff',
+  '#2bff88',
+  '#FC00FF',
+  '#48ded4',
+  '#e82c75',
+  '#ffc40e',
+  '#00A26B'
+];
+
+exports.rand = {
+  color: arrOfColors =>
+    arrOfColors[Math.floor(Math.random() * (arrOfColors.length - 0)) + 0],
+  num: (min, max) => Math.floor(Math.random() * (max - min)) + min
+};
 // exports.menu = [
 //   { slug: '/stores', title: 'Stores', icon: 'store' },
 //   { slug: '/tags', title: 'Tags', icon: 'tag' },
