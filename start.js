642f178f909542fde047cae2ccd694821a54d580
Brian Zelip <bzelip@gmail.com>
2017-11-14 06:29:22 -0500

    port 2600 👾

--- start.js
+++ start.js
@@ -27,7 +27,7 @@ require('./models/Member');
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 7777);
+app.set('port', process.env.PORT || 2600);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
